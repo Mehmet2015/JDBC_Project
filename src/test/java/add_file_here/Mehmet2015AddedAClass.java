5817c29b5fb4000f398badaf2c3f44b463e138e5
Mehmet2015 <72091214+Mehmet2015@users.noreply.github.com>
2020-10-12 22:57:50 +0100

    I added a new file on 12.10.2020

---
+++ src/test/java/add_file_here/Mehmet2015AddedAClass.java
@@ -0,0 +1,20 @@
+package add_file_here;
+
+public class Mehmet2015AddedAClass {
+
+    public static void main(String[] args) {
+        System.out.println("I added a class into JDBC Project");
+        System.out.println("Class name is Mehmet2015AddedAClass");
+        System.out.println("Today is 12.10.2020");
+        //commit your change
+        //with nice commit message
+        //push it back to the github
+        //observe the change
+        //from the your Github repo
+            //click on compare
+        //create a pull request (PR)
+            //to ask original repo owner
+            //to merge your change
+    }
+
+}
